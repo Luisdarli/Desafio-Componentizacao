71d256d936d9b2d69fa162b8cd662219c60c6bf4
Luis Darli <luishenriquedarli@gmail.com>
2021-08-12 15:32:21 -0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 
 import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
 
 // import { SideBar } from './components/SideBar';
 // import { Content } from './components/Content';
@@ -60,22 +62,8 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
 
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
+      <SideBar genres={genres} handleClickButton={handleClickButton} selectedGenreId={selectedGenreId} />
 
       <div className="container">
         <header>
@@ -84,9 +72,7 @@ export function App() {
 
         <main>
           <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
+            <Content movies={movies} />
           </div>
         </main>
       </div>
